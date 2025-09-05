16fd420373178d8567767783553f83fc3005b22f
yyongpall <yyongpall@naver.com>
2025-09-05 16:21:59 +0900

    20250905 16:21

---
+++ Coporation/JEUS/image/JEUS8.5/build.sh
@@ -0,0 +1,1 @@
+docker build -f Dockerfile --no-cache --force-rm -t harbor.hf-tmax.co.kr/library/jeus8.5:v20 .
---
+++ Coporation/JEUS/image/jeus9/build.sh
@@ -0,0 +1,2 @@
+docker build -f Dockerfile.CentOS9xJDK17.0.13xJEUS9000_kube-dns-provider --no-cache --force-rm -t ts20ps:5000/centos9-openjdk-17.0.13-jeus9:9.0.0.0 .
+
